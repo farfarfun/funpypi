57d4ecb5042d97c192d2fa9d7c57fd7dc50dad65
niuliangtao <niuliangtao@qq.com>
2023-10-15 19:15:51 +0800

    add

--- funpypi/version.py
+++ funpypi/version.py
@@ -40,14 +40,14 @@ class VersionManage:
 def read_version(version_path=None, update=False):
     manage = VersionManage(version_path=version_path)
     manage.read()
-    print(os.environ.get("funbuild_multi_index", 0))
-    print(sys.argv)
+    # print(os.environ.get("funbuild_multi_index", 0))
+    # print(sys.argv)
     if update or (
         len(sys.argv) >= 2
         and (os.environ.get("funbuild_multi_index", "0") == "0")
         and (sys.argv[1] == "build" or sys.argv[1] == "bdist_wheel")
     ):
-        #print(sys.argv)
+        # print(sys.argv)
         manage.add()
         manage.write()
     return manage.version
